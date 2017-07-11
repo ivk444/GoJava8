dd3188080af2b63ff8fb7b50e96cf1f1f243fc7c
Ivan Kolesnyk <ivk444@gmail.com>
2017-07-11 15:40:19 +0300

    Initial commit

---
+++ src/JavaTechSkills/Module1.java
@@ -0,0 +1,10 @@
+package JavaTechSkills;
+
+/**
+ * Created by john on 11.07.2017.
+ */
+public class Module1 {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
