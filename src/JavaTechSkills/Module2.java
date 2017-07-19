94688679da70056b46c3ed95a4d1273b921f828d
ivk444 <ivk444@gmail.com>
2017-07-19 18:05:33 +0300

    Module2

---
+++ src/JavaTechSkills/Module2.java
@@ -0,0 +1,38 @@
+package JavaTechSkills;
+
+import java.util.Scanner;
+
+/**
+ * Created by john on 18.07.2017.
+ */
+public class Module2 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        String name = in.nextLine();
+
+        System.out.print("Enter city: ");
+        String city = in.nextLine();
+
+        System.out.print("Enter age: ");
+        int age = in.nextInt();
+
+        System.out.print("Enter hobby: ");
+        in.nextLine();
+        String hobby = in.nextLine();
+
+        System.out.println("\nvar.#1 - table");
+        System.out.println("name: " + name + "\ncity: " + city + "\nage: " + age + "\nhobby: " + hobby);
+
+        System.out.println("\nvar.#2 - text");
+        System.out.println("Человек по имени " + name + " живет в городе " + city + ".\nЭтому человеку " + age + " лет и хобби у него " + hobby + ".");
+
+        System.out.println("\nvar.#3 - other");
+        System.out.println(name + " - name\n" + city + " - city\n" + age + " - age\n" + hobby + " - hobby\n");
+
+    }
+
+}
